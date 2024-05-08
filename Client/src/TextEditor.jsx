74a7abccb4c3eb851cda3d1acf209b316e91f04d
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-05-08 19:02:19 +0530

    added toolbar

--- Client/src/TextEditor.jsx
+++ Client/src/TextEditor.jsx
@@ -2,6 +2,19 @@ import {useCallback, useEffect,useRef} from 'react'
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
 
+const Tool_Bar_Options=[
+  [{header:[1,2,3,4,5,6,false]}],
+  [{font:[]}],
+  [{list:"ordered"},{list:"bullet"}],
+  ["bold","italic","underline"],
+  [{color:[]},{background:[]}],
+  [{script:"sub"},{script:"super"}],
+  [{align:[]}],
+  ["image","blockquote","code-block"],
+  ["clean"],
+  [{ 'direction': 'rtl' }],
+]
+
 export default function TextEditor() {
   
   const wrapperref=useCallback((wrapper)=>{
@@ -10,7 +23,7 @@ export default function TextEditor() {
     wrapper.innerHTML = ""
     const editor=document.createElement("div")
     wrapper.append(editor)
-    new Quill(editor,{theme:"snow"})
+    new Quill(editor,{theme:"snow",modules:{toolbar:Tool_Bar_Options}})
   },[])
   
   return (
