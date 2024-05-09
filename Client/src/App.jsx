5f7f63e3e7187afca5fb01797a9d168e751b5b9a
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-05-09 17:48:16 +0530

    added routes and end points

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,25 @@
 import TextEditor from "./TextEditor"
 import "./styles.css"
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+
+import {v4 as uuidV4} from 'uuid'
+
 function App() {
 
   return (
-   <TextEditor/>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  
+  );
 }
 
 export default App
