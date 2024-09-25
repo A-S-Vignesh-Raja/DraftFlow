72d7846d635afd87532023447ff7689bfcafadd5
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-09-25 16:05:54 +0530

    added render url

--- Client/src/TextEditor.jsx
+++ Client/src/TextEditor.jsx
@@ -52,7 +52,7 @@ export default function TextEditor() {
   ];
 
   useEffect(() => {
-    const s = io("http://localhost:3001");
+    const s = io("https://draftflow.onrender.com");
     setSocket(s);
 
     return () => {
